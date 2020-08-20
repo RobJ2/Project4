3a619c5ea660cf6b6f399c9b84fb3bba3ae8a753
Ubuntu <ubuntu@ip-10-0-0-106.eu-central-1.compute.internal>
2020-08-20 19:15:08 +0000

    update upload_docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,14 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-version=1.1
-dockerpath="04193007/api"
+dockerpath='api'
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-sudo docker login -u USERNAME &&\
-    sudo docker image tag api "${dockerpath}:v2"
+sudo docker login
+sudo docker image tag app "${dockerpath}:v2"
 
 # Step 3:
 # Push image to a docker repository
